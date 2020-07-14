86210c602fb43e65f0cd9c4d2d8afebb6a2f256e
maldevel <maldevel@mail.com>
2020-07-14 17:53:43 +0300

    Update rootend.py
    GPLv3 license header added

--- rootend.py
+++ rootend.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 
+"""
+    rootend - A *nix Enumerator & Auto Privilege Escalation tool.
+    This file is part of rootend Project
+    Written by: @nickvourd
+    Website: https://www.twelvesec.com/
+    GIT: https://github.com/twelvesec/rootend/
+    TwelveSec (@Twelvesec)
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    For more see the file 'LICENSE' for copying permission.
+"""
+
 #Modules area
 import platform
 import distro
